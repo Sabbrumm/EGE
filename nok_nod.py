5bbfb5b91d04b838a57ee28aac94294065365d33
KonyashovNikita <107049717+KonyashovNikita@users.noreply.github.com>
2022-06-11 17:16:37 +0300

    несколько полезных алгоритмов

---
+++ nok_nod.py
@@ -0,0 +1,9 @@
+#функция находит НОД(x, y) двух чисел
+def nod(x, y):
+    x, y = max(x, y), min(x, y)
+    while x > 0 and y > 0:
+        x, y = y, x % y
+    return x
+#функция находит НОК(x, y), строится на базе функции nod(x, y)
+def nok(x, y):
+    return x * y // nod(x, y)
