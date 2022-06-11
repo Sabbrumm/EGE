ac9762b567e6530eb30d9e51c2aa9851234ef7f0
Sabbrumm <38012216+Sabbrumm@users.noreply.github.com>
2022-06-11 17:06:10 +0300

    27 задание ленивое

---
+++ решения/sabbrumm/27-1-s.py
@@ -0,0 +1,15 @@
+file = open('../../задачи/27/27data/1/27-1a.txt')
+
+maxim = 0
+maxim_rzn = 0
+n = int(file.readline())
+for i in range(n):
+    a, b = map(int, file.readline().split())
+    maxim += min(a, b)
+    if abs(a-b)%3 != 0:
+        maxim_rzn = min(maxim_rzn, abs(a-b))
+
+if maxim%3==0:
+    print(maxim+maxim_rzn)
+else:
+    print(maxim)
